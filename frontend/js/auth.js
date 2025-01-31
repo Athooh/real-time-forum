89527f79c9b08ec409c013957c1b5dfe09c4ba81
athooh <s.athooh@gmail.com>
2025-01-31 23:29:33 +0300

    update: added distinct images for login and registration forms

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -5,12 +5,14 @@ const loginForm = document.getElementById('login-form');
 const showLoginLink = document.getElementById('show-login');
 const showRegisterLink = document.getElementById('show-register');
 const forumSection = document.getElementById('forum-section');
+const authImage = document.getElementById('auth-image');
 
 // Show Login Form and Hide Register Form
 showLoginLink.addEventListener('click', (e) => {
     e.preventDefault();
     registerForm.classList.remove('active');
     loginForm.classList.add('active');
+    authImage.src = "./images/register.jpeg";
 });
 
 // Show Register Form and Hide Login Form
@@ -18,10 +20,12 @@ showRegisterLink.addEventListener('click', (e) => {
     e.preventDefault();
     loginForm.classList.remove('active');
     registerForm.classList.add('active');
+    authImage.src = "./images/register.png";
 });
 
 // Initially show the register form
 registerForm.classList.add('active');
+authImage.src = "./images/register.png";
 
 // Handle User Registration
 registerForm.addEventListener('submit', async (e) => {
