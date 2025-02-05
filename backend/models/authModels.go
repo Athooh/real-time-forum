c29083cb0b3ac98f48c079af5c3b5f5ab517fc7e
Raymond9734 <rymadara97@gmail.com>
2025-02-06 00:42:43 +0300

    feat: define User and LoginRequest models for authentication

---
+++ backend/models/authModels.go
@@ -0,0 +1,17 @@
+package models
+
+type User struct {
+	ID        int    `json:"id"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+	Gender    string `json:"gender"`
+}
+
+type LoginRequest struct {
+	Identifier string `json:"identifier"`
+	Password   string `json:"password"`
+}
