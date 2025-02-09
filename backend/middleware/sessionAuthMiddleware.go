a84278c3b9c9d11169aea00fbe29d97cf865a7a3
Raymond9734 <rymadara97@gmail.com>
2025-02-09 15:12:17 +0300

    Add session authentication middleware for secure route protection

---
+++ backend/middleware/sessionAuthMiddleware.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"net/http"
+
+	"forum/backend/controllers"
+	"forum/backend/database"
+	"forum/backend/logger"
+)
+
+// Middleware to check if the user is authenticated
+func SessionAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check if the user is authenticated
+		sessionCookie, err := r.Cookie("session_token")
+		if err != nil || sessionCookie == nil || sessionCookie.Value == "" {
+			logger.Warning("Unauthorized attempt  nil sessionCookie - remote_addr: %s, method: %s, path: %s",
+				r.RemoteAddr,
+				r.Method,
+				r.URL.Path,
+			)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Validating the session token
+		if _, valid := controllers.IsValidSession(database.GloabalDB, sessionCookie.Value); !valid {
+			logger.Warning("Unauthorized attempt  Invalid Session - remote_addr: %s, method: %s, path: %s",
+				r.RemoteAddr,
+				r.Method,
+				r.URL.Path,
+			)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// User is authenticated, call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Middleware chain to apply multiple middleware functions
+func ApplyMiddleware(handler http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+	return handler
+}
