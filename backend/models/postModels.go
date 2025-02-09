efb52bf0c204d41542721a2751a9df272243e3a9
Raymond9734 <rymadara97@gmail.com>
2025-02-10 01:03:13 +0300

    Update Post model to include User and Comments

--- backend/models/postModels.go
+++ backend/models/postModels.go
@@ -7,8 +7,9 @@ import (
 
 type Post struct {
 	ID        int            `json:"id"`
-	Title     string         `json:"title"`
+	User      User           `json:"user"`
 	Author    string         `json:"author"`
+	Title     string         `json:"title"`
 	UserID    int            `json:"user_id"`
 	Category  string         `json:"category"`
 	Likes     int            `json:"likes"`
@@ -17,4 +18,5 @@ type Post struct {
 	Timestamp time.Time      `json:"timestamp"`
 	VideoUrl  sql.NullString `json:"video_url"`
 	Images    []string       `json:"images"`
+	Comments  []Comment      `json:"comments"`
 }
