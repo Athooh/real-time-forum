bf5977d8a2bf8fd013b50b4416bb4c6032cd8830
Raymond9734 <rymadara97@gmail.com>
2025-02-10 01:02:55 +0300

    Update User model with profession and avatar fields

--- backend/models/authModels.go
+++ backend/models/authModels.go
@@ -1,14 +1,16 @@
 package models
 
 type User struct {
-	ID        int    `json:"id"`
-	Nickname  string `json:"nickname"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Age       int    `json:"age"`
-	Gender    string `json:"gender"`
+	ID         int     `json:"id"`
+	Nickname   string  `json:"nickname"`
+	Email      string  `json:"email"`
+	Password   string  `json:"password"`
+	FirstName  string  `json:"first_name"`
+	LastName   string  `json:"last_name"`
+	Age        int     `json:"age"`
+	Gender     string  `json:"gender"`
+	Profession string  `json:"profession"`
+	Avatar     *string `json:"avatar"`
 }
 
 type LoginRequest struct {
