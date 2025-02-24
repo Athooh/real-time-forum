0319392dda3cb943fe5adc94756976d729368685
Raymond9734 <rymadara97@gmail.com>
2025-02-25 00:52:05 +0300

    Modify router navigation to trigger page reload on route change

--- frontend/js/router/router.js
+++ frontend/js/router/router.js
@@ -1,35 +1,35 @@
 class Router {
-    static instance = null;
-    
-    constructor(routes = {}) {
-        if (Router.instance) {
-            return Router.instance;
-        }
-        
-        this.routes = routes;
-        this.currentPath = window.location.pathname;
-        
-        // Handle browser back/forward buttons
-        window.addEventListener('popstate', () => {
-            this.handleRoute(window.location.pathname);
-        });
-        
-        Router.instance = this;
+  static instance = null;
+
+  constructor(routes = {}) {
+    if (Router.instance) {
+      return Router.instance;
     }
 
-    navigate(path) {
-        window.history.pushState({}, '', path);
-        this.handleRoute(path);
-    }
+    this.routes = routes;
+    this.currentPath = window.location.pathname;
 
-    handleRoute(pathname) { 
-        const route = this.routes[pathname] || this.routes['*'];
-        if (route) {
-            route();
-        } else {
-            console.log('No route handler found for:', pathname);
-        }
+    // Handle browser back/forward buttons
+    window.addEventListener("popstate", () => {
+      this.handleRoute(window.location.pathname);
+    });
+
+    Router.instance = this;
+  }
+
+  navigate(path) {
+    window.history.pushState({}, "", path);
+    window.location.reload();
+  }
+
+  handleRoute(pathname) {
+    const route = this.routes[pathname] || this.routes["*"];
+    if (route) {
+      route();
+    } else {
+      console.log("No route handler found for:", pathname);
     }
+  }
 }
 
-export default Router; 
+export default Router;
