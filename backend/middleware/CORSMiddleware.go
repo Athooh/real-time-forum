acfc9ad054e9139267c6f06ea0abc54a1cc77572
rcaleb <calebmadara58@gmail.com>
2025-02-25 14:27:26 +0300

    Enhance CORS middleware to dynamically set allowed origins and improve header management

--- backend/middleware/CORSMiddleware.go
+++ backend/middleware/CORSMiddleware.go
@@ -4,19 +4,33 @@ import "net/http"
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// List of allowed origins
+		// allowedOrigins := []string{
+		// 	"http://localhost:3000",
+		// 	"https://your-production-domain.com",
+		// }
+
+		// Get the request's origin
+		origin := r.Header.Get("Origin")
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		// Check if the origin is allowed
+		// for _, allowedOrigin := range allowedOrigins {
+		// 	if origin == allowedOrigin {
+		// 		w.Header().Set("Access-Control-Allow-Origin", origin)
+		// 		break
+		// 	}
+		// }
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token, content-security-policy")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 		// Handle OPTIONS requests (preflight CORS requests)
 		if r.Method == http.MethodOptions {
-			// w.Header().Set("Access-Control-Allow-Origin", "http://your-frontend-domain.com")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-
-		// Set CORS headers for all responses
-		// w.Header().Set("Access-Control-Allow-Origin")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
